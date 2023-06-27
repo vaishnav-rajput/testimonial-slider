455666719581163a1b910bc1f45879f9ac35c6d1
vaishnav <vaishnav.rajput108@gmail.com>
2023-06-27 16:12:11 +0530

    tailwind css added

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -27,19 +27,23 @@ const Testimonial = (props) => {
         setIndex(randomIndex);
     }
 
-    return(<div>
+    return(<div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center
+    mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md'>
         <Card review={reviews[index]} />
         <div>
-            <div>
-                <button onClick={() => leftShiftHandler(index)}>
+            <div className='flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto px-12'>
+                <button  className='cursor-pointer hover:text-violet-500 ' onClick={() => leftShiftHandler(index)}>
                     <FiChevronLeft/>
                 </button>
-                <button onClick={() => rightShiftHandler(index)}>
+                <button  className='cursor-pointer hover:text-violet-500 ' onClick={() => rightShiftHandler(index)}>
                     <FiChevronRight/>
                 </button>
             </div>
-            <div>
-                <button onClick={surpriseHandler}>
+            <div className="mt-6"> 
+                <button 
+                className='bg-violet-400 hover:bg-violet-500 transition-all duration-200
+                cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'
+                onClick={surpriseHandler}>
                     Surprise Me
                 </button>
             </div>
